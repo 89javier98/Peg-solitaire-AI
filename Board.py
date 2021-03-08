d0f8a6e8693ef79e726298730b95b81a5138461c
Javier Márquez <javier_marquezr@hotmail.es>
2021-03-08 15:01:10 +0100

    First upload
    Idea of the class to be implemented. Still raw, conceptual.

---
+++ Board.py
@@ -0,0 +1,9 @@
+size = 7 
+#Standard size chosen
+
+# void print(): show the board
+# boolean onLimits(int r, int c): position is on limits
+# boolean noMoves(int r, int c): piece can be moved
+# boolean noMoves(): no piece can be moved
+# void move(args?): move a piece (if possible)
+
