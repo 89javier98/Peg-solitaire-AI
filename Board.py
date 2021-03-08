bfa959396a9ea1fa72ea70ab6f82cfad60f2754d
Javier Márquez <javier_marquezr@hotmail.es>
2021-03-08 16:06:41 +0100

    Creation and print of the board created
    The __init__ method creating a list of strings (the rows) and the method to print the board have been implemented (not tested yet)

--- Board.py
+++ Board.py
@@ -1,5 +1,6 @@
-size = 7 
-#Standard size chosen
+# board: variable saving the characters matrix (state of the game)
+
+# Standard self.size chosen
 
 # void print(): show the board
 # boolean onLimits(int r, int c): position is on limits
@@ -7,3 +8,32 @@ size = 7
 # boolean noMoves(): no piece can be moved
 # void move(args?): move a piece (if possible)
 
+#If the self.size = 7
+
+class Board:
+
+    board = []
+    size = 7
+
+    def __init__(self):
+        for r in range(self.size):
+        #Create string
+            row = []
+            for c in range(self.size):
+                rOutOfLimits = r < (self.size-1)/3 or r > (2*(self.size-1))/3
+                cOutOfLimits = c < (self.size-1)/3 or c > (2*(self.size-1))/3
+                if rOutOfLimits and cOutOfLimits:
+                    row.append(' ')
+                else:
+                    row.append('O')
+            self.board.append(row)
+        self.board[3][3] = 'X'
+        
+        
+
+    def print(self):
+        for x in self.board:
+            row = ""
+            for y in x:
+                row = row + y + ' '
+            print(row)
